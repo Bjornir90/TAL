da65b56352d09e6d562940ad48a04c369b1db86c
Bjornir90 <celestincollin@gmail.com>
2020-03-08 18:31:12 +0100

    Add supports for mixed-style column separation in removetags script

--- src/removetags.py
+++ src/removetags.py
@@ -1,10 +1,10 @@
 import argparse
 
 def getArgs():
-    parser = argparse.ArgumentParser(description="Pos_tag text")
-    parser.add_argument("in_file", help="The input file containing the text and the original tags")
-    parser.add_argument("out_file", help="The output file containing the tokens without their corresponding tags")
-    return parser.parse_args()
+	parser = argparse.ArgumentParser(description="Pos_tag text")
+	parser.add_argument("in_file", help="The input file containing the text and the original tags")
+	parser.add_argument("out_file", help="The output file containing the tokens without their corresponding tags")
+	return parser.parse_args()
 	
 args = getArgs()
 in_file = open(args.in_file, "r")
@@ -12,11 +12,14 @@ in_file = open(args.in_file, "r")
 result = ""
 
 for line in in_file:
-    if line == "\n":
-        result += line
-        continue
-    split = line.split("\t")
-    result += split[0] + " "
+	if line == "\n":
+		result += line
+		continue
+	if "\t" in line:
+		split = line.split("\t")
+	else:
+	    split = line.split()
+	result += split[0] + " "
 	
 in_file.close()
 
